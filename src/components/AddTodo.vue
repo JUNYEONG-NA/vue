4909731c602a43e5d799be1dfb8ec6939d8ec9d1
Na-Junyeong <skawns6465@naver.com>
2022-11-18 17:45:38 +0900

    Todo 수정중

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            todo:{
+                id : 1, content : 
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
